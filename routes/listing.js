92d062aa8ff97276e402f1d6e08f4d35872d324f
rahish-pal-23 <rahishpal7795@gmail.com>
2026-09-25 21:19:05 +0530

    Initial clean push

---
+++ routes/listing.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const Listing = require("../models/listing.js");
+const { isloggedIn, isOwner, validateListing } = require("../middleware.js");
+const listingController = require("../controllers/listings.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
+
+router
+  .route("/")
+  .get(wrapAsync(listingController.index))
+
+  .post(
+    isloggedIn,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.createListing),
+  );
+
+// New Route
+
+router.get("/new", isloggedIn, listingController.renderNewForm);
+
+router
+  .route("/:id")
+  .get(wrapAsync(listingController.showListing))
+
+  .put(
+    isloggedIn,
+    isOwner,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.updateListing),
+  )
+
+  .delete(isloggedIn, isOwner, wrapAsync(listingController.destroyListing));
+
+// Edit Route
+router.get(
+  "/:id/edit",
+  isloggedIn,
+  isOwner,
+  wrapAsync(listingController.renderEditForm),
+);
+
+module.exports = router;
